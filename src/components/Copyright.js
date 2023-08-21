17bff2cda9b5d34d96989277d6ef9ce0417b1e68
Chi Sum Simon Hsieh <simon_hsieh@berkeley.edu>
2023-08-20 18:51:04 -0700

    copyright

---
+++ src/components/Copyright.js
@@ -0,0 +1,13 @@
+import ocf from '../ocf-hosted-penguin-dark.png';
+
+function Copyright() {
+    return (
+        <footer className = 'bg-light position-absolute bottom-0 w-100 p-3'>
+            <p> <img src={ocf} className="image-thumbnail pe-5" alt=''/> © 2023 Health and Medical Apprenticeship Program. All Rights Reserved.</p>
+            <p>We are a student group acting independently of the University of California. We take full responsibility for our organization and this web site.</p>
+        </footer>
+
+    );
+};
+
+export default Copyright;
