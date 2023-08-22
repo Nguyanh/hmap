a0a084c91f9944c560cf53df399b0f3d3141f6c7
Jean <jeanye@berkeley.edu>
2023-08-21 17:17:45 -0700

    title screen

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -10,15 +10,8 @@ function Copyright() {
                     <img src={ocf} className="image-thumbnail pe-3" alt=''/>
                     <p>We are a student group acting independently of the University of California. We take full responsibility for our organization and this website.</p>
                 </Container>
-<<<<<<< HEAD
                 <p className="m-0">Built with love and React by <Link to="https://www.linkedin.com/in/nguyentanh/">Thu Nguyen</Link>, <Link to="https://www.linkedin.com/in/simonhsieh123/">Simon Hsieh</Link>, and <Link to ="https://www.linkedin.com/in/jean-ye/">Jean Ye</Link>.</p>
                 <p className="m-0">© 2023 Health and Medical Apprenticeship Program. All Rights Reserved.</p>
-=======
-                <Container className="text-center">
-                    <p className="m-0">Built with love and React by Thu Nguyen and Simon Hseih.</p>
-                    <p className="m-0">© 2023 Health and Medical Apprenticeship Program. All Rights Reserved.</p>
-                </Container>
->>>>>>> ca8afcd094f17d7458feef5357ff337550f26d56
             </Container>
         </footer>
 
